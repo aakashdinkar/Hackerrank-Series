bfd220a95b3497fba76c5a6406f67b5f63b6c0ab
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-19 22:37:20 +0530

    Created Birthday-Cake-Candles.py

---
+++ Birthday-Cake-Candles.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    max_occ = max(ar)
+    count = 0
+    for item in ar:
+        if item == max_occ:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

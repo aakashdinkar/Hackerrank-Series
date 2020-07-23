bf85638a1ede3705055bb28ecb6ef83f2efc0cf2
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-24 01:27:01 +0530

    Created DesignerPDFViewer.py

---
+++ DesignerPDFViewer.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the designerPdfViewer function below.
+def designerPdfViewer(h, word):
+    lst = []
+    for i in word:
+        lst.append(h[ord(i) - 97])
+    max_value = max(lst)
+    return len(word) * max_value
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

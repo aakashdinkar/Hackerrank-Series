36a21f9bf417cd31f79f0df24397391b15c45364
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-24 12:41:53 +0530

    JumpingOnTheClouds.py

---
+++ JumpingOnTheClouds.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    energy = 100
+    for i in range(0, len(c), k):
+        energy -= 1
+        if c[i] == 1:
+            energy -= 2
+    return energy
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()

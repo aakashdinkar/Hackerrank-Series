930abe6b109955cf7de9cff60c0337390ea7d6d5
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-25 20:33:33 +0530

    Created TaumAndBday.py

---
+++ TaumAndBday.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'taumBday' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER b
+#  2. INTEGER w
+#  3. INTEGER bc
+#  4. INTEGER wc
+#  5. INTEGER z
+#
+
+def taumBday(b, w, bc, wc, z):
+    cost_b, cost_w = 0, 0
+    if bc < wc:
+        if bc + z < wc:
+            cost_w = w * (bc + z)
+            cost_b = b * bc
+        else:
+            cost_b = b * bc
+            cost_w = w * wc
+
+    elif wc < bc:
+        if wc + z < bc:
+            cost_b = b * (wc + z)
+            cost_w = w * (wc)
+        else:
+            cost_b = b * bc
+            cost_w = w * wc
+    else:
+        cost_b = b * bc
+        cost_w = w * wc
+
+    return cost_b + cost_w
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        b = int(first_multiple_input[0])
+
+        w = int(first_multiple_input[1])
+
+        second_multiple_input = input().rstrip().split()
+
+        bc = int(second_multiple_input[0])
+
+        wc = int(second_multiple_input[1])
+
+        z = int(second_multiple_input[2])
+
+        result = taumBday(b, w, bc, wc, z)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()

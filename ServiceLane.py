ac921093c64258ba9e71a15cd2f652b968e50bb3
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-26 12:38:55 +0530

    Created ServiceLane.py

---
+++ ServiceLane.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the serviceLane function below.
+def serviceLane(n, cases, width):
+    res = []
+    for item in cases:
+        lst = []
+        for i in range(item[0], item[1]+1):
+            lst.append(width[i])
+        res.append(min(lst))
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nt = input().split()
+
+    n = int(nt[0])
+
+    t = int(nt[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(n, cases, width)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

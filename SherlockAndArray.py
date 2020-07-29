1436863e92b4093ba8ba06bc669db4a83b1ec7b1
Aakash Dinkar <aakashdinkar1307@gmail.com>
2020-07-29 16:04:28 +0530

    SherlockAndArray.py

---
+++ SherlockAndArray.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the balancedSums function below.
+def balancedSums(arr):
+    total = sum(arr)
+    add = 0
+    for item in arr:
+        if add == (total - item - add):
+            return "YES"
+        add += item
+    return "NO"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
